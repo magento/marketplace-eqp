dee8aa98308023171d1027250100121594fafb5a
Lena Orobei <olena.orobei@gmail.com>
2016-09-22 13:04:38 +0300

    TIGER-1241: Implement ObjectManager Sniff (#16)
    * TIGER-1212: Implement sniff that finds count() using to check if array is empty (#4)
    
    - Added ArrayCountSniff
    - Added StrlenSniff
    - Fixed #5, fixed #8
    - Updated README.md
    
    * TIGER-1250: Fixed sniff message
    
    * Tiger-1241: Implement ObjectManager Sniff (#5)

---
+++ MEQP2/Sniffs/Classes/ObjectManagerSniff.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace MEQP2\Sniffs\Classes;
+
+use PHP_CodeSniffer_Sniff;
+use PHP_CodeSniffer_File;
+
+/**
+ * Class ObjectManagerSniff
+ * Detects direct ObjectManager usage.
+ */
+class ObjectManagerSniff implements PHP_CodeSniffer_Sniff
+{
+    /**
+     * String representation of warning.
+     *
+     * @var string
+     */
+    // @codingStandardsIgnoreStart
+    protected $warningMessage = 'The direct use of ObjectManager is discouraged. Inject necessary dependencies via constructor.';
+    // @codingStandardsIgnoreEnd
+
+    /**
+     * Warning violation code.
+     *
+     * @var string
+     */
+    protected $warningCode = 'ObjectManagerFound';
+
+    /**
+     * Possible names of ObjectManager variable or methods from where it can be called.
+     *
+     * @var array
+     */
+    protected $objectManagerNames = [
+        'om',
+        '_om',
+        'objectmanager',
+        '_objectmanager',
+        'getobjectmanager',
+    ];
+
+    /**
+     * ObjectManager methods we are looking for.
+     *
+     * @var array
+     */
+    protected $objectManagerMethods = [
+        'get',
+        'create',
+    ];
+
+    /**
+     * @inheritdoc
+     */
+    public function register()
+    {
+        return [T_OBJECT_OPERATOR, T_DOUBLE_COLON];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+        $methodPosition = $phpcsFile->findNext(T_STRING, $stackPtr + 1);
+        if ($methodPosition !== false &&
+            in_array($tokens[$methodPosition]['content'], $this->objectManagerMethods)
+        ) {
+            $objectManagerPosition = $phpcsFile->findPrevious([T_STRING, T_VARIABLE], $stackPtr - 1);
+            if ($objectManagerPosition !== false) {
+                $objectManagerName = strtolower($tokens[$objectManagerPosition]['content']);
+                if ($tokens[$objectManagerPosition]['code'] === T_VARIABLE) {
+                    $objectManagerName = substr($objectManagerName, 1);
+                }
+                if (in_array($objectManagerName, $this->objectManagerNames)) {
+                    $phpcsFile->addWarning($this->warningMessage, $stackPtr, $this->warningCode);
+                }
+            }
+        }
+    }
+}
---
+++ MEQP2/Tests/Classes/ObjectManagerUnitTest.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+/**
+ * Class MEQP2_Tests_Classes_ObjectManagerUnitTest
+ */
+class MEQP2_Tests_Classes_ObjectManagerUnitTest extends AbstractSniffUnitTest
+{
+    /**
+     * @inheritdoc
+     */
+    public function getErrorList()
+    {
+        return [];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getWarningList()
+    {
+        return [
+            3 => 1,
+            5 => 1,
+            7 => 1,
+            9 => 1,
+            12 => 1,
+            14 => 1,
+            16 => 1,
+            20 => 1,
+            23 => 1,
+        ];
+    }
+}
