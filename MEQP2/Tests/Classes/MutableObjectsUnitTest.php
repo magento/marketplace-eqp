a4f7b536c3f6e8cda5ed88cbb68e6b27c0a66688
Magento Extension Quality Program Coding Standard <pepsi_08@list.ru>
2016-09-29 18:01:01 +0300

    TIGER-1214 : Implement sniff that finds if Request, Session and Cookie objects are requested in constructor (#6)
    * added MutableObjectsSniff.php, MutableObjectsUnitTest.inc, MutableObjectsUnitTest.php
    
    * Made changes to MEQP2/Sniffs/Classes/MutableObjectsSniff.php, MEQP2/Tests/Classes/MutableObjectsUnitTest.inc, MEQP2/Tests/Classes/MutableObjectsUnitTest.php
    
    * use \Magento\Framework\Search\Request\Config as SearchRequestConfig; -should not be found
    
    * Changed test files
    
    * Added case Magento\Framework\Session\SessionManagerInterface AS Manager; should not be a warning
    
    * Corrected with PSR2 standard
    
    * Corrected tests
    
    * Last changes.
    
    * Corrected MEQP2/Sniffs/Classes/MutableObjectsSniff.php
    
    * Removed redundant variable from MEQP2/Sniffs/Classes/MutableObjectsSniff.php

---
+++ MEQP2/Tests/Classes/MutableObjectsUnitTest.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+/**
+ * Class MEQP2_Tests_ClassesMutableObjectsUnitTest
+ */
+class MEQP2_Tests_Classes_MutableObjectsUnitTest extends AbstractSniffUnitTest
+{
+    /**
+     * @inheritdoc
+     */
+    public function getErrorList()
+    {
+        return [];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getWarningList()
+    {
+        return [
+            26 => 1,
+            27 => 1,
+            31 => 1,
+            33 => 1,
+            34 => 1,
+            70 => 1
+        ];
+    }
+}
