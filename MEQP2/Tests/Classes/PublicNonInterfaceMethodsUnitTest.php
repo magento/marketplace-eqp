8f5f347c1994979d48b0cedb1a94b36e2177bb27
Lena Orobei <olena.orobei@gmail.com>
2016-10-20 12:09:12 +0300

    MM-214: Implement sniff that finds all non-interface public methods in actions and observers (#12)

---
+++ MEQP2/Tests/Classes/PublicNonInterfaceMethodsUnitTest.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+/**
+ * Class MEQP2_Tests_Classes_PublicNonInterfaceMethodsUnitTest
+ */
+class MEQP2_Tests_Classes_PublicNonInterfaceMethodsUnitTest extends AbstractSniffUnitTest
+{
+    /**
+     * @inheritdoc
+     */
+    public function getErrorList()
+    {
+        return [];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getWarningList($testFile = '')
+    {
+        switch ($testFile) {
+            case 'PublicNonInterfaceMethodsUnitTest.Controller.inc':
+                return [
+                    23 => 1,
+                ];
+                break;
+            case 'PublicNonInterfaceMethodsUnitTest.Observer.inc':
+                return [
+                    18 => 1,
+                ];
+                break;
+            case 'PublicNonInterfaceMethodsUnitTest.Abstract.inc':
+            default:
+                return [];
+                break;
+        }
+    }
+}
