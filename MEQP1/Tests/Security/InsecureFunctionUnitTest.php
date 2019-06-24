690b6c93c7877da38bd412929512465f47dc6732
Serhii Litviachenko <slitviachenko@magento.com>
2019-06-24 12:16:51 -0500

    REPO-317: [EQP] Add insecure functions sniff with severity 10 for M1 packages - created a new InsecureFunction sniff identical to the corresponding M2 sniff

---
+++ MEQP1/Tests/Security/InsecureFunctionUnitTest.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Copyright © Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace MEQP1\Tests\Security;
+
+use PHP_CodeSniffer\Tests\Standards\AbstractSniffUnitTest;
+
+/**
+ * Class InsecureFunctionUnitTest
+ */
+class InsecureFunctionUnitTest extends AbstractSniffUnitTest
+{
+    /**
+     * @inheritdoc
+     */
+    public function  getErrorList()
+    {
+        return [
+            3 => 1,
+            5 => 1,
+            7 => 1,
+            9 => 1,
+            11 => 1,
+            13 => 1,
+            15 => 1,
+            17 => 1,
+            19 => 1,
+            21 => 1,
+            23 => 1,
+            25 => 1,
+            27 => 1,
+            29 => 1,
+            31 => 1,
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getWarningList()
+    {
+        return [];
+    }
+}
